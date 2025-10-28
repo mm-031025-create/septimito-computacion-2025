1cba0ec0395074a32484772d96c5268f6aa1a8f2
mm-031025-create <mm-031025@liceobarcelona.net>
2025-10-28 10:26:07 -0600

    Add files via upload

---
+++ ejercicio1 (1).py
@@ -0,0 +1,4 @@
+a = int(input("ingresa el primer numero hijo:"))
+a = int(input("ingresa el segundo numero hijo:"))
+c = int(input("ingresa el tercero hijo:"))
+print("La suma es:",a + b + c)                                   
